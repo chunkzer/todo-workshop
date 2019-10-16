59b248f85d95d852af9c39f75bab326b779ab527
Christian <christian.ruink@neplusultra.io>
2019-10-16 15:17:38 -0700

    Todo lister initial state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import closeIcon from './close.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppTitle > TODO LISTER 9000</AppTitle>
+        <TodoInputContainer>
+          <TodoInput />
+          <AddTodoButton >New</AddTodoButton>
+        </TodoInputContainer>
+        <TodoListContainer >
+          <Todo>
+            <TodoText>Termina la documentacion del workshop</TodoText>
+            <DeleteIcon />
+          </Todo>
+          <Todo>
+            <TodoText>Llamale a tu mama</TodoText>
+            <DeleteIcon />
+          </Todo>
+          <Todo>
+            <TodoText>Haz todas tus vueltas burocraticas</TodoText>
+            <DeleteIcon />
+          </Todo>
+          <Todo>
+            <TodoText>Agenda una limpieza dental</TodoText>
+            <DeleteIcon />
+          </Todo>
+        </TodoListContainer >
+      </Header>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const TodoInput = styled.input`
+  width: 150px;
+`;
+
+const AppTitle = styled.div`
+  margin-top: 150px;
+  font-size: 48px;
+  color: teal;
+`;
+
+const TodoInputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 50px;
+`;
+
+const AddTodoButton = styled.div`
+  background-color: blue;
+  color: white;
+  height: 20px;
+  width: 90px;
+  cursor: pointer;
+  font-size: 18px;
+`;
+
+const TodoListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TodoText = styled.div`
+  margin-left: 4px;
+  color: white;
+  font-size: 18px;
+`;
+
+const Todo = styled.div`
+  margin-top: 10px;
+  width: 1000px;
+  border: 3px solid white;
+  height: 25px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DeleteIcon = styled.img.attrs({ src: closeIcon })`
+  cursor: pointer;
+  width: 30px;
+  height: 13px;
+  margin-right: 4px;
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default App;
